049d8dd71ff074b740c2eb5c7d0ef89fd3aaf8dc
davidschmidt <schmdt628@gmail.com>
2014-09-30 11:45:04 -0400

    working dictionary import

--- proj1/college.php
+++ proj1/college.php
@@ -1,24 +1,31 @@
 <?php
-
+//import csv
 class importcsv{
 function importcsv($file){
 $first_row = TRUE;
+$records = array();
 ini_set('auto_detect_line_endings',TRUE);
-if (($handle = fopen($file, "r")) !== FALSE) {
+  if (($handle = fopen($file, "r")) !== FALSE) {
     while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
-     if($first_row == TRUE) {
-       $column_heading = $row;
-       $first_row = FALSE;
-      } else {
-       $record = array_combine($column_heading, $row);
-       $records[] = $record;
-     }
+       if($first_row == TRUE) {
+           $column_heading = $row;
+           $first_row = FALSE;
+       } else {
+           $record = array_combine($column_heading, $row);
+           $records[] = $record;
+       }
 
       
-    }
+   }
 
     fclose($handle);
 }
+new csvrecord($records);
+}
+}
+
+class csvrecord{
+function csvrecord($records){
   foreach($records as $record) {
     foreach($record as $key => $value) {
       echo $key . ': ' . $value .  "</br> \n";
@@ -27,23 +34,6 @@ if (($handle = fopen($file, "r")) !== FALSE) {
   }
 }
 }
-
-
-$file = "test.csv";
-new importcsv($file);
-
-
-
-
-foreach($records as $record) {
-    foreach($record as $key => $value) {
-      echo $key . ': ' . $value .  "</br> \n";
-    }   
-    echo '<hr>';
-  }
-
-//import csv
-
 
 
 
@@ -56,7 +46,10 @@ foreach($records as $record) {
 //link
 
 
+new importcsv("test.csv");
 
+new importcsv("var.csv");
+//new csvrecord($records);
 
 
 ?>

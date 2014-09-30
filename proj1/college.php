87ac420460be31ba2e0818e5a342dd070ad8b39e
davidschmidt <schmdt628@gmail.com>
2014-09-30 10:30:31 -0400

    added csv parse code

--- proj1/college.php
+++ proj1/college.php
@@ -1,6 +1,22 @@
 <?php
 
 //import csv
+function fileImport($fileName){ 
+     ini_set('auto_detect_line_endings' , TRUE);
+     if(($handle = fopen($fileName, 'r')) !== FALSE){
+        echo "handle returned";     
+	print_r($handle);
+        return $handle;
+     }
+     echo "handle closed";
+     fclose($handle);
+    }
+
+   function fclose($handle) {
+    
+   
+   }
+
 class fileImport{
    function fileImport($fileName){ 
      ini_set('auto_detect_line_endings' , TRUE);
@@ -19,14 +35,52 @@ class fileImport{
    }
 }
 
-merge csv array
+class csvRead {
+  function csvRead($handle) {
+    $first_row = TRUE;
+    while(($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+       if($first_row == TRUE) {
+         $column_heading = $row;
+ 
+         $first_row = FALSE;
+       }else{
+         $record = array_combine($column_heading, $row);
+         $records[] = $record;
+	 return  $records[];
+	 
+       }
+   
+   }
+    }
+   
+}
+
+class keyConvert {
+  function keyConvert($records[]) {
+//    for each($records as $cord) {
+//      for each ($cord as $key=> $value) {
+//        echo $key . ': ' . $value . "<br> \n";
+	echo "splat";
+//	}
+//   }
+  }
+}
 
 
-constructor
+//file name -> import
+$fileName = 'test.csv';
+new fileImport($fileName);
+new csvRead($handle);
+
+new keyConvert($records[]);
+//merge csv array
+
+
+//constructor
 
 
 
-link
+//link
 
 
 

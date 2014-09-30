be47ca00ba15f46fbdb2de2cfa646998765047d2
davidschmidt <schmdt628@gmail.com>
2014-09-30 09:55:11 -0400

    csv import

--- proj1/college.php
+++ proj1/college.php
@@ -1,7 +1,23 @@
 <?php
 
-import csv
+//import csv
+class fileImport{
+   function fileImport($fileName){ 
+     ini_set('auto_detect_line_endings' , TRUE);
+     if(($handle = fopen($fileName, 'r')) !== FALSE){
+        echo "handle returned";     
+	print_r($handle);
+        return $handle;
+     }
+     echo "handle closed";
+     fclose($handle);
+    }
 
+   function fclose($handle) {
+    
+   
+   }
+}
 
 merge csv array
 

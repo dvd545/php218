e3f50665a1dd80fffae222619d9e16ef5a433518
davidschmidt <schmdt628@gmail.com>
2014-09-30 10:56:38 -0400

    attempt to revert to oob

--- proj1/college.php
+++ proj1/college.php
@@ -1,77 +1,60 @@
 <?php
 
-//import csv
-function fileImport($fileName){ 
-     ini_set('auto_detect_line_endings' , TRUE);
-     if(($handle = fopen($fileName, 'r')) !== FALSE){
-        echo "handle returned";     
-	print_r($handle);
-        return $handle;
+
+class csvimport{
+function csvimport($file){
+$first_row = TRUE;
+ini_set('auto_detect_line_endings',TRUE);
+if (($handle = fopen($file, "r")) !== FALSE) {
+    while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+     if($first_row == TRUE) {
+       $column_heading = $row;
+       $first_row = FALSE;
+     } else {
+       $record = array_combine($column_heading, $row);
+       $records[] = $record;
      }
-     echo "handle closed";
-     fclose($handle);
+
+      
     }
 
-   
-
-class fileImport{
-   function fileImport($fileName){ 
-     ini_set('auto_detect_line_endings' , TRUE);
-     if(($handle = fopen($fileName, 'r')) !== FALSE){
-        echo "handle returned";     
-	print_r($handle);
-        return $handle;
-     }
-     echo "handle closed";
-     fclose($handle);
-    }
-
-   function fclose($handle) {
-    
-   
-   }
+    fclose($handle);
+}
+}
 }
 
-class csvRead {
-  function csvRead($handle) {
-    $first_row = TRUE;
-    while(($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
-       if($first_row == TRUE) {
-         $column_heading = $row;
- 
-         $first_row = FALSE;
-       }else{
-         $record = array_combine($column_heading, $row);
-         $records[] = $record;
-	 return  $records;
-	 
-       }
-   
-   }
+class keyInsert{
+function keyInsert($records)
+  foreach($records as $record) {
+    foreach($record as $key => $value) {
+      echo $key . ': ' . $value .  "</br> \n";
     }
-   
-}
-
-class keyConvert {
-  function keyConvert($records) {
-//    for each($records as $cord) {
-//      for each ($cord as $key=> $value) {
-//        echo $key . ': ' . $value . "<br> \n";
-	echo "splat";
-//	}
-//   }
+    echo '<hr>';
   }
 }
+}
 
 
-//file name -> import
-$fileName = 'test.csv';
-new fileImport($fileName);
-new csvRead($handle);
+$file = "test.csv";
+new csvimport($file);
+new keyInsert($records);
 
-new keyConvert($records);
+
+
+
+foreach($records as $record) {
+    foreach($record as $key => $value) {
+      echo $key . ': ' . $value .  "</br> \n";
+    }   
+    echo '<hr>';
+  }
+
+//import csv
+
+
+
+
 //merge csv array
-
 
 //constructor
 

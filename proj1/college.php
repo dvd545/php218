1aec2ad84af12ba6cacc511d1ac75ef96967b1e4
davidschmidt <schmdt628@gmail.com>
2014-10-02 19:06:36 -0400

    added comments

--- proj1/college.php
+++ proj1/college.php
@@ -1,106 +1,82 @@
 <?php
-//import csv
+
 class challenge{
-public function importcsv($file){
-  $first_row = TRUE;
-  $records = array();
-  ini_set('auto_detect_line_endings',TRUE);
-  if (($handle = fopen($file, "r")) !== FALSE) {
-    while (($row = fgetcsv($handle, 4096, ",")) !== FALSE) {
-      if($first_row == TRUE) {
+
+  public function importcsv($file){ //file import
+    $first_row = TRUE;
+    $records = array();
+    ini_set('auto_detect_line_endings',TRUE);
+    if (($handle = fopen($file, "r")) !== FALSE) {
+      while (($row = fgetcsv($handle, 4096, ",")) !== FALSE) {
+        if($first_row == TRUE) {
            $column_heading = $row;
            $first_row = FALSE;
-       } else {
+        } else {
            $record = array_combine($column_heading, $row);
            $records[] = $record;
        }
+      }
+      fclose($handle);
    }
-    fclose($handle);
-}
 
-return $records;
-}
-    
-    
-public function cleaner($records){
-    foreach($records as $key => $value) {
-     echo $key . ': ' . $value .  "</br> \n";
-        
-    }    
+   return $records;
+ }
 
-}
-
-
-  public function school_link($school_records){
-      $r_name = array_column($school_records, 'INSTNM');
+ public function school_link($school_records){ //creates links
+    $r_name = array_column($school_records, 'INSTNM');
     if(empty($_GET)) {
         $i = 1;
       foreach($school_records as $school_record) {
           static_html::links($school_records, $i, $r_name);
-          $i++;
-        
+          $i++;  
+       }
+     }
   }
-}
-}
 
 
 } //class close
 
 class static_html{
-  static public function links($school_records, $i, $r_name){
-        $school_record_num = $i - 1;
-       echo '<a href=' . '"http://localhost/php218/proj1/college.php?school_record=' .               $school_record_num . '"' . '>' . $r_name[$i] . ' </a>';
+  static public function links($school_records, $i, $r_name){ //link creation function
+     $school_record_num = $i - 1;
+     echo '<a href=' . '"http://localhost/php218/proj1/college.php?school_record=' .               $school_record_num . '"' . '>' . $r_name[$i] . ' </a>';
 
      echo '</p>';
   }
-  static public function table($vals){
-    echo "<table border = 1 bordercolor= black cellspacing=0 cellpadding=5 style='font-size:14pt'>";
-echo "<tr>";
-
-
-
-
-foreach($vals as $key => $value){
-   echo '<th>', $key, '</th>';
-   echo '<td>', $value, '</td>';
-    echo '</tr>';
-}
-
-echo '</table>';
-
-
-  }    
+  
     
+  static public function table($vals){ //table creation function
+       echo "<table border = 1 bordercolor= black cellspacing=0 cellpadding=5 style='font-size:14pt'>";
+     echo "<tr>";
 
+
+    foreach($vals as $key => $value){
+        echo '<th>', $key, '</th>';
+        echo '<td>', $value, '</td>';
+        echo '</tr>';
+      }
+
+     echo '</table>';
+
+   }    
+    
 }
 
-$obj = new challenge;
 
-$myrecords = $obj->importcsv("var.csv");
-$schools = $obj->importcsv("hd2.csv");
-$variables = array_column($myrecords, 'varTitle', 'varname' );
 
-$display = $obj->school_link($schools);
+$obj = new challenge; //create new object
+
+$myrecords = $obj->importcsv("var.csv"); //import first csv into array
+$schools = $obj->importcsv("hd2.csv"); //import secont csv into seperate array
+
+$variables = array_column($myrecords, 'varTitle', 'varname' ); 
+//take varTitle and varname out of dictionary
+
+$display = $obj->school_link($schools); //creates links 
 $school = $schools[$_GET['school_record']];
 
-$vals= array_combine($variables, $school);
-//$vals = $obj->cleaner($vals);
-$table = static_html::table($vals)
+$vals= array_combine($variables, $school); //combines arrays into one
+$table = static_html::table($vals)  //create table
 
-/*		echo "<table border = 1 bordercolor= black cellspacing=0 cellpadding=5 style='font-size:14pt'>";
-echo "<tr>";
-
-
-
-
-foreach($vals as $key => $value){
-   echo '<th>', $key, '</th>';
-   echo '<td>', $value, '</td>';
-    echo '</tr>';
-}
-
-echo '</table>';
-
-*/
 
 ?>
